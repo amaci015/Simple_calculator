e91be58d5931ef3c8f347daf9c1b0950330f76b6
Andrew Maciel <amaci015uottawa.ca>
2023-09-26 11:59:18 -0400

    Initial commit

---
+++ app/src/main/java/andrewmaciel/com/example/simplecalculatorseg2105/MainActivity.kt
@@ -0,0 +1,18 @@
+package andrewmaciel.com.example.simplecalculatorseg2105;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+public class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
